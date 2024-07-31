51e56fc187df6da62adab6a08f6467c21a85598b
tiagolimar <tiagolima.r2@gmail.com>
2024-07-30 21:56:56 -0300

    add controller

---
+++ src/controller/cadastro/cadastro.php
@@ -0,0 +1,12 @@
+<?php
+function cadastrar() {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    echo $nome, $email, $telefone;
+}
+
+if ($_POST){
+    cadastrar();
+}
+?>
