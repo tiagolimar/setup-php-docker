baa81f12e59443631203c6289d4017addada9e8b
tiagolimar <tiagolima.r2@gmail.com>
2024-07-30 21:58:02 -0300

    editing index.php and remove files

--- public/index.php
+++ public/index.php
@@ -1,5 +1,34 @@
 <?php
 
-echo "<h1 class='text-center'>Olá mundo</h1><hr>";
+$routes = [
+    '/listar' => '../src/views/listar/listar.php',
+    '/cadastro' => '../src/views/cadastro/cadastro.php',
+];
 
-include "../src/table.php";
+$controller = [
+    '/listar' => '../src/controller/listar/listar.php',
+    '/cadastro' => '../src/controller/cadastro/cadastro.php',
+];
+
+function direcionarRota($routes, $controller) {
+    $url = $_SERVER['REQUEST_URI'];
+    $naoEncontrado = TRUE;
+    
+    foreach ($routes as $key => $value) {
+        if ($url == $key) {
+            include $value;
+            include $controller[$key];
+            $naoEncontrado = FALSE;
+        }
+    }
+    
+    if($naoEncontrado == TRUE) echo 'Página não encontrada';
+}
+
+function renderView($routes, $controller){
+    include '../src/views/_template/head.php';
+    direcionarRota($routes, $controller);
+    include '../src/views/_template/footer.php';
+}
+
+renderView($routes, $controller);
